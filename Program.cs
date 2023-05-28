f1167d5f0c896f2987b44a99851d2984370c25fe
RZT <renzhitong329@163.com>
2023-05-28 19:32:19 +0800

    追加镜像导出筛选功能

--- Program.cs
+++ Program.cs
@@ -4,14 +4,16 @@ using System.Text.RegularExpressions;
 using X.Common.Helper;
 using System.Runtime.InteropServices;
 
-bool compress = args.Length > 0 && args[0] == "compress";
+//bool compress = args.Length > 0 && args[0] == "compress";
+bool compress = false;
 string imgTag = RuntimeInformation.ProcessArchitecture == Architecture.X64 ? ":amd64" : string.Empty;
 Console.WriteLine($"s: save images {Environment.NewLine}l: load images");
 string? option = Console.ReadLine();
+
 if (option == "s")
 {
     var output = CommandHelper.Execute("docker", "image ls");
-    var images = output.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries).Select(x =>
+    List<Image> images = output.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries).Select(x =>
     {
         var split = x.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
         return new Image(split[0], split[1], split[2]);
@@ -20,30 +22,74 @@ if (option == "s")
     foreach (var image in images)
         Console.WriteLine($"{count++,-2} : {image.Repo,-35}{image.Tag,-20}\t{image.Id,-20}");
 
-    Console.WriteLine("Select images to export, use space to seperate");
-    string? input = Console.ReadLine();
-    if (string.IsNullOrWhiteSpace(input))
-        input = string.Join(" ", Enumerable.Range(1, images.Count - 1).Select(x => x.ToString()));
-    var selected = Regex.Split(input, @"\s+").Select(x => int.Parse(x)).ToArray();
-    Parallel.ForEach(selected, index =>
-    // foreach (var index in selected)
+
+    Console.WriteLine("是否采用7z进行压缩(默认为否)请输入 y、n");
+    string? isCompress = Console.ReadLine();
+    if (isCompress == "y" && !string.IsNullOrWhiteSpace(isCompress))
     {
-        var image = images[index];
-        string name = $"{image.Repo}:{image.Tag}";
-        string fileName = $"{Path.GetFileName(image.Repo)}-{image.Tag}";
-        Console.WriteLine($"Exporting {name}");
-        CommandHelper.Execute("docker", $"save {name} -o {fileName}.tar");
-        if (compress)
+        compress = true;
+    }
+
+    Console.WriteLine("Select images to export, use space to seperate");
+
+    string? input = Console.ReadLine();
+
+    //if (string.IsNullOrWhiteSpace(input))
+    //    input = string.Join(" ", Enumerable.Range(1, images.Count - 1).Select(x => x.ToString()));
+
+    if (!string.IsNullOrWhiteSpace(input))
+    {
+        bool isAllDigits = Regex.IsMatch(input, @"^\d+$");
+
+        if (isAllDigits)
         {
-            Console.WriteLine($"Exported {name}. Compressing...");
-            string compressResult = CommandHelper.Execute("docker", $"run --rm -v {Environment.CurrentDirectory}:/app 7z{imgTag} a -mx9 -bsp1 -sdel {fileName}.7z {fileName}.tar");
-            Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
-            Console.WriteLine(compressResult);
-            Console.WriteLine("Compressed");
+            // 根据数字进行ID的精确匹配
+            //var selectedImages = images.Where(image => image.Id == number.ToString()).ToArray();
+
+            // 输出选中的图像信息
+            Console.WriteLine("Selected images:");
+
+            int[] selected = Regex.Split(input, @"\s+").Select(x => int.Parse(x)).ToArray();
+            exportImages(selected, images);
         }
         else
-            Console.WriteLine($"Exported {name}. ");
-    });
+        {
+            // 根据repo进行模糊匹配
+            var filteredImages = images.Where(image => image.Repo.Contains(input));
+            var matchingIndexes = images
+                                    .Select((image, index) => new { Image = image, Index = index })
+                                    .Where(item => item.Image.Repo.Contains(input))
+                                    .Select(item => item.Index)
+                                    .ToList();
+            int count2 = 0;
+            foreach (var image in filteredImages)
+                Console.WriteLine($"{matchingIndexes[count2++],-2} : {image.Repo,-35}{image.Tag,-20}\t{image.Id,-20}");
+
+            string? subInput = "";
+            while (true)
+            {
+                // 输出选中的图像信息
+                Console.WriteLine("请输入选择的id(不支持全选):");
+                subInput = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(subInput))
+                {
+                    break;
+                }
+            }
+
+            var selected = Regex.Split(subInput, @"\s+").Select(x => int.Parse(x)).ToArray();
+            exportImages(selected, images);
+        }
+    }
+    else
+    {
+        Console.WriteLine("No input entered. Showing all images:");
+        // 使用所有图像进行后续处理，与上述代码类似
+        input = string.Join(" ", Enumerable.Range(1, images.Count - 1).Select(x => x.ToString()));
+        var selected = Regex.Split(input, @"\s+").Select(x => int.Parse(x)).ToArray();
+        exportImages(selected, images);
+    }
 
 }
 else if (option == "l")
@@ -72,8 +118,29 @@ else if (option == "l")
 }
 
 
-
-Console.WriteLine("Hello, World!");
+void exportImages(int[] selected, List<Image> images)
+{
+    Parallel.ForEach(selected, index =>
+    // foreach (var index in selected)
+    {
+        var image = images[index];
+        string name = $"{image.Repo}:{image.Tag}";
+        string fileName = $"{Path.GetFileName(image.Repo)}-{image.Tag}";
+        Console.WriteLine($"Exporting {name}");
+        CommandHelper.Execute("docker", $"save {name} -o {fileName}.tar");
+        if (compress)
+        {
+            Console.WriteLine($"Exported {name}. Compressing...");
+            string compressResult = CommandHelper.Execute("docker", $"run --rm -v {Environment.CurrentDirectory}:/app 7z{imgTag} a -mx9 -bsp1 -sdel {fileName}.7z {fileName}.tar");
+            Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
+            Console.WriteLine(compressResult);
+            Console.WriteLine("Compressed");
+        }
+        else
+            Console.WriteLine($"Exported {name}. ");
+    });
+}
+//Console.WriteLine("Hello, World!");
 
 public class Image
 {
--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Save and load docker images in bulk.
 
 ### Publish
-    $ dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --self-contained -p:PublishTrimmed=true -p:PublishReadyToRun=true -p:PublishReadyToRunComposite=true
-    $ dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeNativeLibrariesForSelfExtract=true  -p:PublishReadyToRun=true -p:PublishReadyToRunComposite=true
-    $ dotnet publish -c Release -r osx.11.0-arm64 --no-self-contained -p:PublishSingleFile=true
+```cmd
+dotnet publish -c Release -r linux-x64 --self-contained -p:PublishSingleFile=true -p:PublishTrimmed=true -p:PublishReadyToRun=true -p:PublishReadyToRunComposite=true
+dotnet publish -c Release -r win-x64 --self-contained -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeNativeLibrariesForSelfExtract=true  -p:PublishReadyToRun=true -p:PublishReadyToRunComposite=true
+dotnet publish -c Release -r osx.11.0-arm64 --no-self-contained -p:PublishSingleFile=true
+```
